b63ec2f43da50c793d0bdb0443f64c5afa39e6cd
Cory Witt <cpwitt@mail.mccneb.edu>
2025-07-26 15:23:23 -0500

    Update

--- libs/lib_convert.js
+++ libs/lib_convert.js
@@ -1,6 +1,6 @@
 /*
 Cory Witt
-script.js
+lib_convert.js
 INFO 1579
 Shaw
 07/26/2025
@@ -9,30 +9,29 @@ Shaw
 "use strict";
 
 //SuperDate class definition. SuperDate extends the Date class 
-//for defining short names for month and day
-//as well as full names fir month and day.
+//for defining short and full names for month and day.
+
 
 class SuperDate extends Date {  
-    constructor(date) {
+    constructor(date) {           // Constructor for SuperDate class
         if (date == undefined) {  // If no date is provided, use the current date
-            date = new Date();
             super();    
         } else {                  // If a date is provided, use it to create a new instance
             super(date);
         }
     }
-    getFullDayName() {  // Returns the full name of the day (predefined array containing day names of the week )
-        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        return days[this.getDay()];
+    getFullDayName() {             // Returns the full name of the day by using the getDay() method as an index to a predefined array of day names
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        return daysOfWeek[this.getDay()];   
     }
-    getShortDayName() { // Returns the first three letters of the day name (substring 0 to 3)
+    getShortDayName() {            // Returns the first three letters of the day name (substring 0 to 3) by using the getFullDayName method
         return this.getFullDayName().substring(0, 3); 
     }
-    getFullMonthName() {  // Returns the full name of the month (predefined array containing month names)
+    getFullMonthName() {           // Returns the full name of the month by using the getMonth() method as an index to a predefined array of month names
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         return months[this.getMonth()];
     }
-    getShortMonthName() {   // Returns the first three letters of the month name (substring 0 to 3)
+    getShortMonthName() {          // Returns the first three letters of the month name (substring 0 to 3) by using the getFullMonthName method
         return this.getFullMonthName().substring(0, 3);
     }
 
--- script.js
+++ script.js
@@ -3,77 +3,78 @@ Cory Witt
 script.js
 INFO 1579
 Shaw
-07/24/2025
+07/27/2025
 */
  
+//Date-a-Lator program allows the user to input a date
+//and displays the full and short names of the day and month.
 
 "use strict";
-window.addEventListener('load', function() {
-    const userInput = document.getElementById('userInput');
-    const action = document.getElementById('action');
-    const resultHolder = document.getElementById('resultHolder');
-    let superDate;
+window.addEventListener('load', function() {                               //Ensure the DOM is fully loaded 
+    const userInput = document.getElementById('userInput');                //Reference the input field for date.
+    const action = document.getElementById('action');                      //Reference the action button
+    const resultHolder = document.getElementById('resultHolder');          //Reference the result holder output
+    let superDate;   
 
-    function checkValue(str, max) {
+    function checkValue(str, max) {                                        // Function validates and formats that the input value
         if (str.charAt(0) !== '0' || str == '00') {
             let num = parseInt(str);
-            if (isNaN(num) || num <= 0 || num > max) num = 1;
-            str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
+            if (isNaN(num) || num <= 0 || num > max) num = 1;              // If the input is not numeric, less than or equal to 0, or greater than max, set it to 1 
+            str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();   
         };
         return str;
     };
 
-    userInput.addEventListener('input', function(e) {
+    userInput.addEventListener('input', function(e) {                      // Event listener for input changes. Formats the input and validates the date.
         const field = e.currentTarget;
         field.type = 'text';
-        let input = field.value;
-        if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
-        let values = input.split('/').map(function(v) {
+        let input = field.value;                                           
+        if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);  
+        let values = input.split('/').map(function(v) {                   
             return v.replace(/\D/g, '');
         });
-        if (values[0]) values[0] = checkValue(values[0], 12);
-        if (values[1]) values[1] = checkValue(values[1], 31);
+        if (values[0]) values[0] = checkValue(values[0], 12);             // Validates and format the month value by calling the checkValue() function
+        if (values[1]) values[1] = checkValue(values[1], 31);             // Validates and format the day value by calling the checkValue() function
         let output = values.map(function(v, i) {
           return v.length == 2 && i < 2 ? v + ' / ' : v;
         });
         field.value = output.join('').substr(0, 14);
-        if(input == "") resultHolder.innerHTML = "";
+        if(input == "") resultHolder.innerHTML = "";                      // If the input is empty, clear the result holder.
     });
 
-    userInput.addEventListener('blur', function(e) {
+    userInput.addEventListener('blur', function(e) {                      // Event listener for when the input field loses focus.
         const field = e.currentTarget;
         field.type = 'text';
         let input = field.value;
-        let values = input.split('/').map(function(v, i) {
+        let values = input.split('/').map(function(v, i) {  
           return v.replace(/\D/g, '')
         });
         let output = '';
         
-        if (values.length == 3) {
+        if (values.length == 3) {                                        // If there are three values (month, day, year), assign them to variables, and create a new Date object by calling the Date constructor with year, month, and day.
           let year = values[2].length !== 4 ? parseInt(values[2]) + 2000 : parseInt(values[2]);
           let month = parseInt(values[0]) - 1;
           let day = parseInt(values[1]);
           let d = new Date(year, month, day);
-          if (!isNaN(d)) {
-            let dates = [d.getMonth() + 1, d.getDate(), d.getFullYear()];
-            output = dates.map(function(v) {
+          if (!isNaN(d)) {                                                   // If the date is valid, format the output to display the date.
+            let dates = [d.getMonth() + 1, d.getDate(), d.getFullYear()];    // Get the month, date, and year from the SuperDate methods getMonth, getDate, and getFullYear
+            output = dates.map(function(v) {                                 // output result is set.
               v = v.toString();
               return v.length == 1 ? '0' + v : v;
             }).join(' / ');
-            superDate = new SuperDate(d);
+            superDate = new SuperDate(d);                                 
           };
         };
-        field.value = output;
+        field.value = output;                                                // Set the input field to the formatted output result.
       });
 
-      action.addEventListener('click', function(e) {
-        const currentDate = new Date();
-        let verb = superDate.getFullYear() > currentDate.getFullYear() ? "will take" : "took";
-        
-        if (superDate != undefined) {
-            const s = `
+    action.addEventListener('click', function(e) {                           // Event listener for the action button click. 
+        const currentDate = new Date();                                                           // Get the current date
+        let verb = superDate.getFullYear() > currentDate.getFullYear() ? "will take" : "took";    // Calls the SuperDate method getFullYear to compare superDate year with current year to determine if the event is future or past. Since only the year is compared, the year has to be greater than the current year to be "will take"
+        if (superDate != undefined) {                                                             // If superDate is defined, display the formatted date information by calling the SuperDate methods getfullDayName, getShortDayName, getFullMonthName, and getShortMonthName
+            const s = `                                                                            
                 <h2>Date-A-Late:</h2>
-                <div>This event ${verb} place on a ${superDate.getFullDayName()} (${superDate.getShortDayName()})</div>
+                <div>This event ${verb} place on a ${superDate.getFullDayName()} (${superDate.getShortDayName()})</div>   
                 <div>in the month of ${superDate.getFullMonthName()} (${superDate.getShortMonthName()}).</div>
             `
             resultHolder.innerHTML = s;
